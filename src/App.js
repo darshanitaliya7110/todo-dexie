422702e139257e15723995d8f4e0cc4b3f60d19e
darshanitaliya7110 <darshan.italiya@pixer.digital>
2024-10-01 11:15:34 +0530

    implement add task functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react'
-import './App.css'
+import React from "react";
+import "./App.css";
+import Dexie from "dexie";
+import { useLiveQuery } from "dexie-react-hooks";
+
+const db = new Dexie("todoApp");
+db.version(1).stores({
+  todos: "++id,task,completed",
+});
+
+const { todos } = db;
 
 const App = () => {
+  const allItems = useLiveQuery(() => todos.toArray());
+  console.log(">>>>>>>", allItems);
+
+  const addTask = async (e) => {
+    e.preventDefault();
+    const taskFeild = document.querySelector("#taskInput");
+
+    await todos.add({
+      task: taskFeild["value"],
+      completed: false,
+    });
+
+    taskFeild["value"] = "";
+  };
+
   return (
     <div className="container">
       <h3 className="teal-text center-align">Todo App</h3>
-      <form className="add-item-form">
+      <form className="add-item-form" onSubmit={addTask}>
         <input
           type="text"
+          id="taskInput"
           className="itemField"
           placeholder="What do you want to do today?"
           required
@@ -19,29 +44,25 @@ const App = () => {
 
       <div className="card white darken-1">
         <div className="card-content">
-          <div className="row">
-            <p className="col s10">
-              <label>
-                <input type="checkbox" checked className="checkbox-blue" />
-                <span className="black-tex strike-text">Call John Legend</span>
-              </label>
-            </p>
-            <i className="col s2 material-icons delete-button">delete</i>
-          </div>
-
-          <div className="row">
-            <p className="col s10">
-              <label>
-                <input type="checkbox" className="checkbox-blue" />
-                <span className="black-tex">Do my laundry</span>
-              </label>
-            </p>
-            <i className="col s2 material-icons delete-button">delete</i>
-          </div>
+          {allItems?.map(({ id, completed, task }) => (
+            <div className="row" key={id}>
+              <p className="col s10">
+                <label>
+                  <input
+                    type="checkbox"
+                    checked={completed}
+                    className="checkbox-blue"
+                  />
+                  <span className="black-tex strike-text">{task}</span>
+                </label>
+              </p>
+              <i className="col s2 material-icons delete-button">delete</i>
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
